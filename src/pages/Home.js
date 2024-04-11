38994e76a006e8ae69e02584372cc5bdced6eca4
sanjaraiy <deepakrai78788@gmail.com>
2024-04-11 22:22:58 +0530

    added pages in client side

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../features/navbar/NavBar'
+import ProductList from '../features/product-list/ProductList'
+function Home() {
+  return (
+    <NavBar>
+        <ProductList></ProductList>
+    </NavBar>
+  )
+}
+
+export default Home
