aceb78dd2815096b0abbde5c1c4681295ecbc9a7
sanjaraiy <deepakrai78788@gmail.com>
2024-04-11 23:03:59 +0530

    added routing in Login and Signup Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/home"></Navigate>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/signin' element={<LoginPage></LoginPage>}></Route>
+        <Route path='/signup' element={<SignupPage></SignupPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
